01b6b80d3c0137fd51f20a064f0e72a7d24a7b8b
Doug Forester <dforester@binaryanvil.com>
2018-09-11 10:55:17 -0700

    Boilerplate
     Boilerplate Commit

---
+++ blocks/idx_block_name_1/view.php
@@ -0,0 +1,16 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+
+/*
+ * This file is part of IDX Publisher.
+ *
+ * (c) Doug Forester <doug.forester@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+?>
+<div class="block-boilerplate">
+    <span>IDX Publisher</span>
+</div>
